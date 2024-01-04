efcd17cbd423d3f441c97de939e202224178fb6e
Andi Maulana <88288325+andimaulana18@users.noreply.github.com>
2024-01-04 12:17:38 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+const notesContainer = document.querySelector(".notes-container");
+const createButton = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+function showNotes(params) {
+    notesContainer.innerHTML = localStorage.getItem("notes"); 
+}
+showNotes();
+
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+createButton.addEventListener("click", ()=>{
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", "true");
+    img.src = "assets/img/delete.png";
+    notesContainer.appendChild(inputBox).appendChild(img);
+});
+
+
+notesContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "IMG") {
+        e.target.parentElement.remove();
+        updateStorage();
+    } else if (e.target.tagName === "P") {
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function(params) {
+                updateStorage();
+            }
+        })
+    }
+});
+
+document.addEventListener("keydown", event =>{
+    if (event.key === "Enter") {
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+});
